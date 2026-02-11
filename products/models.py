f850d0fea9577d14240cbd28316e9684e366de58
hicelery <mungoharrison.ward9@gmail.com>
2026-02-11 16:18:21 +0000

    add meta to category for decoding

--- products/models.py
+++ products/models.py
@@ -50,4 +50,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ['name']
+        ordering = ['category_id']
+
+    def __str__(self):
+        return f"{self.name}"
